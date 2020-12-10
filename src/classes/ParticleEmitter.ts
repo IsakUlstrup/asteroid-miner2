49887d1121d7956218de76fdd8a839afe5eaec88
Isak <isakulstrup@gmail.com>
2020-12-10 09:51:07 +0100

    added particleemitter class

---
+++ src/classes/ParticleEmitter.ts
@@ -0,0 +1,27 @@
+import GameObject from "./GameObject";
+import Particle from "./Particle";
+
+export default class ParticleEmitter extends GameObject {
+  particles: Particle[];
+  constructor(transform: Vector2) {
+    super(transform);
+    this.particles = [];
+  }
+
+  public emit(transform = this.transform) {
+    this.particles.push(new Particle(transform));
+  }
+  public update(dt: number) {
+    this.particles.forEach((p) => {
+      p.update(dt);
+    });
+    this.particles = this.particles.filter((p) => {
+      p.opacity > 0;
+    });
+  }
+  public draw(context: CanvasRenderingContext2D, cameraPosition: Vector2) {
+    this.particles.forEach((p) => {
+      p.draw(context, cameraPosition);
+    });
+  }
+}
